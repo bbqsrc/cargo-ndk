5b2e6e5424fc2f4868f1a50b8666fee8b5a9f961
Brendan Molloy <brendan@bbqsrc.net>
2021-01-09 14:34:12 +0100

    Set default log filter to info

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod cli;
 mod meta;
 
 fn main() {
-    env_logger::from_env(env_logger::Env::default().default_filter_or("trace")).init();
+    env_logger::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     if env::var("CARGO").is_err() {
         eprintln!("This binary may only be called via `cargo ndk`.");
