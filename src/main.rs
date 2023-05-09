213c0d68b2522df6daa976f7705bb21dde85cc07
Brendan Molloy <brendan@bbqsrc.net>
2023-05-09 23:10:21 +0200

    Create an abomination
    So there's a bug where NDK r25 doesn't handle command line args
    properly to clang. Which is precisely what rustc calls, and breaks.
    
    Highly paid teams at Google in their infinite wisdom implemented
    argument handling with [reads notes] wizardry, wishful thinking,
    and ... fucking batch scripts in 2023. `.cmd` files. For arg parsing! ON
    WINDOWS! WHAT IN THE NAME OF ZOMBIE JESUS. Anyway.
    
    So now cargo-ndk will use the actual Win32 function for parsing args
    before handing them off to their cursed batch files. It took 2 hours to
    workaround this issue. The bug in the NDK repo has been open for 3
    months.
    
    I should invoice Google at this point.
    
    https://github.com/android/ndk/issues/1856

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,17 @@ mod cargo;
 mod cli;
 mod meta;
 
-fn main() {
+#[cfg(windows)]
+fn args_hack(cmd: &str) -> anyhow::Result<()> {
+    let args = wargs::command_line_to_argv(None)
+        .skip(1)
+        .collect::<Vec<_>>();
+    let mut process = std::process::Command::new(cmd).args(args).spawn()?;
+
+    Ok(process.wait().map(|_| ())?)
+}
+
+fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     if env::var("CARGO").is_err() {
@@ -13,7 +23,24 @@ fn main() {
         exit(1);
     }
 
+    #[cfg(windows)]
+    {
+        let main_arg = std::env::args().next().unwrap();
+        let main_arg = std::path::Path::new(&main_arg)
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .unwrap();
+
+        if main_arg != "cargo-ndk" {
+            let maybe = main_arg.to_uppercase().replace('-', "_");
+            let app = std::env::var(format!("CARGO_NDK_{maybe}"))?;
+            return args_hack(&app);
+        }
+    }
+
     let args = std::env::args().skip(2).collect::<Vec<_>>();
 
     cli::run(args);
+    Ok(())
 }
